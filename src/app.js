ebb9770053286f2f094b4e87b302e79bf7938d4c
Diego Rodriguez <diegorodriguez93@hotmail.com>
2021-09-22 18:42:40 -0300

    trying another stackoverflow solution

--- src/app.js
+++ src/app.js
@@ -6,14 +6,28 @@ let stream = require("./ws/stream");
 let path = require("path");
 let favicon = require("serve-favicon"); */
 
-const express = require("express");
+/* const express = require("express");
 var app = express();
 const PORT = process.env.PORT || 80;
 var server = app.listen(PORT);
 var io = require("socket.io").listen(server);
 let stream = require("./ws/stream");
 let path = require("path");
+let favicon = require("serve-favicon"); */
+const express = require("express");
+const PORT = process.env.PORT || 80;
+
+const app = express();
+const socketIO = require("socket.io");
+let stream = require("./ws/stream");
+let path = require("path");
 let favicon = require("serve-favicon");
+
+const server = express()
+  .use(app)
+  .listen(PORT, () => console.log(`Listening Socket on ${PORT}`));
+
+const io = socketIO(server);
 
 app.use(favicon(path.join(__dirname, "favicon.ico")));
 app.use("/assets", express.static(path.join(__dirname, "assets")));
@@ -24,10 +38,6 @@ app.get("/", (req, res) => {
 
 app.get("/user", (req, res) => {
   res.sendFile(__dirname + "/user.html");
-});
-
-app.get("/socket.io/socket.io.js", (req, res) => {
-  res.sendFile(__dirname + "/socket.io/socket.io.js");
 });
 
 io.of("/stream").on("connection", stream);
